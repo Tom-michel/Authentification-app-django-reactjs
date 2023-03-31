e66396b1b0b53b2cbb14b1c5038e267ce1505662
Tom-michel <rufin.btompe@facsciences-uy1.cm>
2023-03-31 22:34:51 +0100

    api completed and fontend app created

---
+++ auth_api/api/serializers.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model, authenticate
+
+UserModel = get_user_model()
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('__all__')
+
+    def create(self, clean_data):
+        user_obj = UserModel.objects.create_user(
+            email=clean_data['email'],  password=clean_data['password'])
+        user_obj.username = clean_data['username']
+        user_obj.save()
+        return user_obj
+
+
+class UserLoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField()
+
+    def check_user(self, clean_data):
+        email = clean_data['email']
+        password = clean_data['password']
+        user = authenticate(username=email, password=password)
+        if not user:
+            raise serializers.ValidationError('Invalid Credentials')
+        return user
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserModel
+        fields = ('username', 'email')
